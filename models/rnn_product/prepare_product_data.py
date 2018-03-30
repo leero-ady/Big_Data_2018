1d09698748a845a3fbb9d5b4aa566e7b5e194dc2
Madhav <kasamsaimadhavk@gmail.com>
2018-03-30 03:31:09 -0400

    Added the remaining part of prepare_product_data.py and completed it.

--- models/rnn_product/prepare_product_data.py
+++ models/rnn_product/prepare_product_data.py
@@ -63,4 +63,45 @@ if __name__ == '__main__':
     product_name_length = np.zeros(shape=[num_rows], dtype=np.int8)
     history_length = np.zeros(shape=[num_rows], dtype=np.int8)
 
-    
+    for i, row in product_data.iterrows():
+        if i % 10000 == 0:
+            print i, num_rows
+
+        user_id[i] = row['user_id']
+        product_id[i] = row['product_id']
+        aisle_id[i] = row['aisle_id']
+        department_id[i] = row['department_id']
+        eval_set[i] = row['eval_set']
+        label[i] = row['label']
+
+        is_ordered_history[i, :], history_length[i] = pad_1d(map(int, row['is_ordered_history'].split()), 100)
+        index_in_order_history[i, :], _ = pad_1d(map(int, row['index_in_order_history'].split()), 100)
+        order_dow_history[i, :], _ = pad_1d(map(int, row['order_dow_history'].split()), 100)
+        order_hour_history[i, :], _ = pad_1d(map(int, row['order_hour_history'].split()), 100)
+        days_since_prior_order_history[i, :], _ = pad_1d(map(int, row['days_since_prior_order_history'].split()), 100)
+        order_size_history[i, :], _ = pad_1d(map(int, row['order_size_history'].split()), 100)
+        reorder_size_history[i, :], _ = pad_1d(map(int, row['reorder_size_history'].split()), 100)
+        order_number_history[i, :], _ = pad_1d(map(int, row['order_number_history'].split()), 100)
+        product_name[i, :], product_name_length[i] = pad_1d(map(int, row['product_name_encoded'].split()), 30)
+
+    if not os.path.isdir('data'):
+        os.makedirs('data')
+
+    np.save('data/user_id.npy', user_id)
+    np.save('data/product_id.npy', product_id)
+    np.save('data/aisle_id.npy', aisle_id)
+    np.save('data/department_id.npy', department_id)
+    np.save('data/eval_set.npy', eval_set)
+    np.save('data/label.npy', label)
+
+    np.save('data/is_ordered_history.npy', is_ordered_history)
+    np.save('data/index_in_order_history.npy', index_in_order_history)
+    np.save('data/order_dow_history.npy', order_dow_history)
+    np.save('data/order_hour_history.npy', order_hour_history)
+    np.save('data/days_since_prior_order_history.npy', days_since_prior_order_history)
+    np.save('data/order_size_history.npy', order_size_history)
+    np.save('data/reorder_size_history.npy', reorder_size_history)
+    np.save('data/order_number_history.npy', order_number_history)
+    np.save('data/product_name.npy', product_name)
+    np.save('data/product_name_length.npy', product_name_length)
+    np.save('data/history_length.npy', history_length)
